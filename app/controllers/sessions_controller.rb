fff9924b3f2798af9b0d92b51a75c5ed7c27e4cf
tatar3n <97883444+Tatar3n@users.noreply.github.com>
2025-01-09 01:05:18 +0300

    auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,20 @@ class SessionsController < ApplicationController
   def new
   end
   def create
-    user = User.find_by_email(auth_params[:email])
-    if user && user.authenticate(auth_params[:password])
-      session[:current_user_id] = user.id
+    @user = User.find_by_email(auth_params[:email])
+    if @user && @user.authenticate(auth_params[:password])
+      session[:current_user_id] = @user.id
       redirect_to root_path
     else
       redirect_to login_path
     end
   end
+
+  def destroy
+    session[:current_user_id] = nil
+    redirect_to root_path
+  end
+
   private
   def auth_params
     params.require(:user).permit(:email, :password)
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
   post "/register", to: "users#create"
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
-  get "/logout", to: "sessions#destroy"
+  delete "/logout", to: "sessions#destroy"
 end
