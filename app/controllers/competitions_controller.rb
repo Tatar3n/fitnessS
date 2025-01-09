14f4ddc5acaee21a7e1fbff83033e7af68372963
tatar3n <97883444+Tatar3n@users.noreply.github.com>
2025-01-09 19:30:07 +0300

    fix deploy and tests

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -10,12 +10,16 @@ class CompetitionsController < ApplicationController
   def new
     @competition = Competition.new
   end
+
   def create
   end
+
   def edit
   end
+
   def update
   end
+
   def destroy
   end
 end
